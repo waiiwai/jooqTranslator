7160921486f91896c141bf680bde05397808e519
Y.Iwai <85144602+waiiwai@users.noreply.github.com>
2021-11-16 08:33:57 +0900

    関数追加

--- js/translate.js
+++ js/translate.js
@@ -1,21 +1,53 @@
 String.prototype.select = function(...columns){
-    return this + 'select ' + columns;
+    return this + ' select ' + columns;
 }
-String.prototype.insertInto = function(table){
-    return this + 'insert into ' + table;
+
+String.prototype.insertInto = function(table, ...columns){
+    return this + ' insert into ' + table + '(' + columns + ')';
 }
+String.prototype.values = function(...values){
+    return this + ' values (' + values + ')';
+}
+
 String.prototype.update = function(table){
-    return this + 'update ' + table;
+    return this + ' update ' + table;
 }
-String.prototype.delete = function(table){
-    return this + 'delete ' + table;
+String.prototype.set = function(col, val){
+    if (this.indexOf('set') >= 0) return this + ' , ' + col + '=' + val;
+    else return this + ' set ' + col + '=' + val;
+}
+String.prototype.deleteFrom = function(table){
+    return this + ' delete from ' + table;
 }
 String.prototype.from = function(table){
     return this + ' from ' + table;
 }
+String.prototype.join = function(table){
+    return this + ' join ' + table;
+}
+String.prototype.on = function(cond){
+    return this + ' on ' + cond;
+}
+
+String.prototype.where = function(cond){
+    return this + ' where ' + cond;
+}
+String.prototype.and = function(cond){
+    return this + ' and ' + cond;
+}
+String.prototype.or = function(cond){
+    return this + ' or ' + cond;
+}
+String.prototype.in = function(...values){
+    return this + ' in ' + values;
+}
+
+String.prototype.replaceNames =function() {
+    return this;
+}
 
 function trans() {
-    let i  = document.getElementById('inputData').value;
+    let i  = document.getElementById('inputData').value.replaceAll(' ', '').replaceNames();
     let f = '"use strict";return ("".' + i + ')';
     let o = Function(f)();
     document.getElementById('outputData').value = o;
