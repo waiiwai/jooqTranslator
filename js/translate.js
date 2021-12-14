5fad9bc0d3346b6bc259567076a6904c5bea7b1c
Y.Iwai <85144602+waiiwai@users.noreply.github.com>
2021-12-14 19:43:44 +0900

    キャメルケースをスネークケースに変換

--- js/translate.js
+++ js/translate.js
@@ -1,29 +1,29 @@
 String.prototype.select = function(...columns){
-    return this + ' select ' + columns;
+    return this + ' select ' + columns.map(column => column.camel2Snake());
 }
 
 String.prototype.insertInto = function(table, ...columns){
-    return this + ' insert into ' + table + '(' + columns + ')';
+    return this + ' insert into ' + table + '(' + columns.map(column => column.camel2Snake()) + ')';
 }
 String.prototype.values = function(...values){
     return this + ' values (' + values + ')';
 }
 
 String.prototype.update = function(table){
-    return this + ' update ' + table;
+    return this + ' update ' + table.camel2Snake();
 }
 String.prototype.set = function(col, val){
     if (this.indexOf('set') >= 0) return this + ' , ' + col + '=' + val;
     else return this + ' set ' + col + '=' + val;
 }
 String.prototype.deleteFrom = function(table){
-    return this + ' delete from ' + table;
+    return this + ' delete from ' + table.camel2Snake();
 }
 String.prototype.from = function(table){
-    return this + ' from ' + table;
+    return this + ' from ' + table.camel2Snake();
 }
 String.prototype.join = function(table){
-    return this + ' join ' + table;
+    return this + ' join ' + table.camel2Snake();
 }
 String.prototype.on = function(cond){
     return this + ' on ' + cond;
@@ -42,14 +42,21 @@ String.prototype.in = function(...values){
     return this + ' in ' + values;
 }
 
-String.prototype.replaceNames =function() {
-    return this;
+String.prototype.replaceArgs = function() {
+    return this.replaceAll("(", "('").replaceAll(")", "')").replaceAll(",", "','");
+}
+
+String.prototype.camel2Snake = function() {
+    return this.replace(/([A-Z])/g, (p0) => {return '_' + p0.toLowerCase();});
+}
+
+String.prototype.snake2Camel = function() {
+    return this.replace(/(_[a-z])/g, (p0) => {return p0.substring(1).toUpperCase();});
 }
 
 function trans() {
-    let i  = document.getElementById('inputData').value.replaceAll(' ', '').replaceNames();
+    let i  = document.getElementById('inputData').value.replaceAll(' ', '').replaceArgs();
     let f = '"use strict";return ("".' + i + ')';
     let o = Function(f)();
     document.getElementById('outputData').value = o;
 }
-
