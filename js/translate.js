e98267227dc8cf9337d2684b1d7b359f2e80ff1f
Y.Iwai <85144602+waiiwai@users.noreply.github.com>
2021-12-15 09:49:12 +0900

    バッククォート付加

--- js/translate.js
+++ js/translate.js
@@ -1,29 +1,29 @@
 String.prototype.select = function(...columns){
-    return this + ' select ' + columns.map(column => column.camel2Snake());
+    return this + ' select ' + columns.map(column => column.camel2Snake().addBackQuote());
 }
 
 String.prototype.insertInto = function(table, ...columns){
-    return this + ' insert into ' + table + '(' + columns.map(column => column.camel2Snake()) + ')';
+    return this + ' insert into ' + table + '(' + columns.map(column => column.camel2Snake().addBackQuote()) + ')';
 }
 String.prototype.values = function(...values){
     return this + ' values (' + values + ')';
 }
 
 String.prototype.update = function(table){
-    return this + ' update ' + table.camel2Snake();
+    return this + ' update ' + table.camel2Snake().addBackQuote();
 }
 String.prototype.set = function(col, val){
     if (this.indexOf('set') >= 0) return this + ' , ' + col + '=' + val;
     else return this + ' set ' + col + '=' + val;
 }
 String.prototype.deleteFrom = function(table){
-    return this + ' delete from ' + table.camel2Snake();
+    return this + ' delete from ' + table.camel2Snake().addBackQuote();
 }
 String.prototype.from = function(table){
-    return this + ' from ' + table.camel2Snake();
+    return this + ' from ' + table.camel2Snake().addBackQuote();
 }
 String.prototype.join = function(table){
-    return this + ' join ' + table.camel2Snake();
+    return this + ' join ' + table.camel2Snake().addBackQuote();
 }
 String.prototype.on = function(cond){
     return this + ' on ' + cond;
@@ -52,6 +52,10 @@ String.prototype.camel2Snake = function() {
 
 String.prototype.snake2Camel = function() {
     return this.replace(/(_[a-z])/g, (p0) => {return p0.substring(1).toUpperCase();});
+}
+
+String.prototype.addBackQuote = function() {
+    return '`' + this.replaceAll('.', '`.`') + '`';
 }
 
 function trans() {
